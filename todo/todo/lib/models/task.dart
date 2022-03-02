2f5b2a32c451cd6f620ace265bc9d134645056f3
Clément Boulet <clement.boulet60510@gmail.com>
2022-03-02 15:52:45 +0100

    Correction

--- todo/todo/lib/models/task.dart
+++ todo/todo/lib/models/task.dart
@@ -1,14 +1,11 @@
 class Task {
   int id;
   String content;
-  bool completed;
-  DateTime createdAt;
+  bool completed = false;
+  DateTime createdAt = DateTime.now();
 
   // Constructor, with syntactic sugar for assignment to members.
-  Task(this.id, this.content, this.completed, this.createdAt) {
-    completed = false;
-    createdAt = DateTime.now();
-  }
+  Task(this.id, this.content, this.completed, this.createdAt);
 
   // Named constructor that forwards to the default one.
   //Task.unlaunched(String name) : this(name, null);
