3d787f04f74734f1469ccda325ad7ce41faf48c6
Clément Boulet <clement.boulet60510@gmail.com>
2022-03-15 12:19:29 +0100

    Suppression d'une tâche

--- todo/todo/lib/screens/all_tasks.dart
+++ todo/todo/lib/screens/all_tasks.dart
@@ -16,6 +16,7 @@ class AllTasks extends StatefulWidget {
 
 class _AllTasksState extends State<AllTasks> {
   Task? selectedTask;
+  bool showSnackBar = false;
 
   void showDetails(Task task) {
     setState(() {
@@ -29,6 +30,21 @@ class _AllTasksState extends State<AllTasks> {
     });
   }
 
+  void deleteTask(Task task) {
+    data.tasks.remove(task);
+    hideDetails();
+  }
+
+  void confirmDelete(Task task) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: const Text('Voulez-vous vraiment supprimer cette tâche ?'),
+      action: SnackBarAction(
+        label: 'Oui',
+        onPressed: () => deleteTask(task),
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +56,11 @@ class _AllTasksState extends State<AllTasks> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               selectedTask != null
-                  ? TaskDetails(task: selectedTask, hideDetails: hideDetails)
+                  ? TaskDetails(
+                      task: selectedTask,
+                      hideDetails: hideDetails,
+                      confirmDelete: confirmDelete,
+                    )
                   : Container(),
               TaskMaster(
                 tasks: data.tasks,
